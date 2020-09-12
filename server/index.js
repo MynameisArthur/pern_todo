aff71a66e7cdeb5ad01c498cd9ca3b1912b07158
Arthur <arthurd800@wp.pl>
2020-09-12 16:00:27 +0200

    modified README, added get and put routes

--- server/index.js
+++ server/index.js
@@ -16,7 +16,43 @@ app.post('/todos', async (req, res) => {
             'INSERT INTO todo (description) VALUES ($1) RETURNING *',
             [description]
         );
-        res.status(200).json({success: true, msg: newTodo});
+        res.status(200).json({success: true, msg: newTodo.rows[0]});
+    } catch (err) {
+        console.error(err.message);
+    }
+});
+
+app.get('/todos', async (req, res) => {
+    try {
+        const todos = await pool.query('SELECT * FROM todo');
+        res.status(200).json({success: true, msg: todos});
+    } catch (err) {
+        console.error(err.message);
+    }
+});
+
+app.get('/todos/:id', async (req, res) => {
+    try {
+        const todo = await pool.query('SELECT * FROM todo WHERE todo_id=$1', [
+            req.params.id,
+        ]);
+        res.status(200).json({success: true, msg: todo});
+    } catch (err) {
+        console.error(err.message);
+    }
+});
+
+app.put('/todos/:id', async (req, res) => {
+    try {
+        const {description} = req.body;
+        const todo = await pool.query(
+            'UPDATE todo SET description = $1 WHERE todo_id = $2 RETURNING *',
+            [description, req.params.id]
+        );
+        res.status(200).json({
+            success: true,
+            data: {msg: 'todo updated', todo},
+        });
     } catch (err) {
         console.error(err.message);
     }
